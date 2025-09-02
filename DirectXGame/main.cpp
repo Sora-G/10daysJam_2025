5311d8d164eed0a18cf70097ff30208b3dd4aa55
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-09-02 22:18:35 +0900

    mainに実行可能な機能を追加

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,9 +1,27 @@
 #include <Windows.h>
 #include "KamataEngine.h"
-#include "SceneManager.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"Player");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		dxCommon->PreDraw();
+
+
+
+		dxCommon->PostDraw();
+	}
+	KamataEngine::Finalize();
 
 	return 0;
 }
