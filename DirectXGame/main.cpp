0ac2110f80ffea0b36f28eb9ce2f4bfd3550247f
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-09-02 22:11:07 +0900

    ・SceneManagerでWorldTranform.cppが見つからないエラーが出ているためmainからSceneManager関連を削除

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,10 +4,6 @@
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	//シーンマネージャーを宣言
-	SceneManager* sceneManager = new SceneManager();
-	//実行
-	sceneManager->Run();
 
 	return 0;
 }
