705733340c05c614583e20948d3ae0a79a80d62e
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-09-02 21:21:39 +0900

    TitleSceneの追加

---
+++ DirectXGame/TitleScene.cpp
@@ -0,0 +1,13 @@
+#include "TitleScene.h"
+
+TitleScene::~TitleScene() {}
+
+void TitleScene::Init() {}
+
+void TitleScene::Update() {}
+
+void TitleScene::DrawBackGroundSprite() {}
+
+void TitleScene::DrawForeGroundSprite() {}
+
+void TitleScene::DrawModel() {}
