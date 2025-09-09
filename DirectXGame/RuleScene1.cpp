99bb4caa809206d8250227ae1895eda9ae2853af
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-09-09 16:23:25 +0900

    GameClear,GameOver,RuleScene1 2の各シーンを追加

---
+++ DirectXGame/RuleScene1.cpp
@@ -0,0 +1,13 @@
+#include "RuleScene1.h"
+
+RuleScene1::~RuleScene1() {}
+
+void RuleScene1::Init() {}
+
+void RuleScene1::Update() {}
+
+void RuleScene1::DrawBackGroundSprite() {}
+
+void RuleScene1::DrawForeGroundSprite() {}
+
+void RuleScene1::DrawModel() {}
---
+++ DirectXGame/RuleScene2.cpp
@@ -0,0 +1,13 @@
+#include "RuleScene2.h"
+
+RuleScene2::~RuleScene2() {}
+
+void RuleScene2::Init() {}
+
+void RuleScene2::Update() {}
+
+void RuleScene2::DrawBackGroundSprite() {}
+
+void RuleScene2::DrawForeGroundSprite() {}
+
+void RuleScene2::DrawModel() {}
